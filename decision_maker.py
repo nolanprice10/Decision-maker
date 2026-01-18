f71271b4d05c41e89d9994ab88b220a76a04d2fd
Nolan Price <nolangp10@gmail.com>
2026-01-17 22:27:01 -0500

    Add decision maker functionality in decision_maker.py
    Implement decision maker that randomly selects from user inputs.

---
+++ decision_maker.py
@@ -0,0 +1,15 @@
+import random
+
+decisions = []
+
+while True:
+
+  choice = input("type a decision; type DONE to finish: ")
+  if choice == "DONE":
+    break
+decisions.append(choice)
+if len(decisions) == 0:
+  print("you have to actually add a decision lol"
+  exit()
+result = random.choice(decisions)
+print("how about...", result)
